87fd6cc8ccfd576e33522c16cad63aaffff908b7
sherry0451 <37391494+sherry0451@users.noreply.github.com>
2020-05-21 15:31:53 -0600

    Create tem.py

---
+++ tem.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# author：S.Yao time:20-05-21
+
+# print("PyTorch Version: ", torch.__version__)
+# print('GPU is available:', torch.cuda.is_available())
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
